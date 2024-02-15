c1c4ed9df7879b3d650603be25c4fd763cfbdf2b
praveenpriyashan <praveenpriyashan@gmail.com>
2024-02-15 16:12:36 +0530

    hiber2

--- src/entity/StudentEntity.java
+++ src/entity/StudentEntity.java
@@ -19,23 +19,23 @@ import java.util.List;
 @ToString
 public class StudentEntity {
 
-//    @Id
-//    @Column(name = "id",nullable = false)
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//
-//    private StudentName studentName ;
-//
-//    @Column(name = "nic",nullable = false,length = 12)
-//    @Nullable
-//    private String nic;
-//
-//    @CreationTimestamp
-//    @Column(name = "Create_date",nullable = false)
-//    private Date createDate;
-//
-//
-//    @ElementCollection
-//    @CollectionTable(name = "student_mobile",joinColumns = @JoinColumn(name = "student_id"))
-//    private List<String> mobiles;
+    @Id
+    @Column(name = "id",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private StudentName studentName ;
+
+    @Column(name = "nic",nullable = false,length = 12)
+    @Nullable
+    private String nic;
+
+    @CreationTimestamp
+    @Column(name = "Create_date",nullable = false)
+    private Date createDate;
+
+
+    @ElementCollection
+    @CollectionTable(name = "student_mobile",joinColumns = @JoinColumn(name = "student_id"))
+    private List<String> mobiles;
 }
